df2ed7dfe92099ee0cee33d83c8f9e8484818fa4
Josh Ludahl <josh.ludahl@gmail.com>
2020-07-22 10:29:27 -0700

    updated app configurations

--- app/src/main/java/com/android/elk/common/TestConfiguration.kt
+++ app/src/main/java/com/android/elk/common/TestConfiguration.kt
@@ -1,8 +1,31 @@
 package com.android.elk.common
 
+import android.app.Instrumentation
 import android.content.Context
+import android.content.res.AssetManager
 import androidx.test.platform.app.InstrumentationRegistry
 
+/**
+ * Application context.
+ */
 val targetContext: Context get() = InstrumentationRegistry.getInstrumentation().targetContext
 
+/**
+ * Instrumentation context
+ */
+val testContext: Context get() = InstrumentationRegistry.getInstrumentation().context
+
+/**
+ * AssetManager for androidTest environment
+ */
+val testAssets: AssetManager = testContext.assets
+
+/**
+ * Test runner instrumentation
+ */
+val instrumentation: Instrumentation get() = InstrumentationRegistry.getInstrumentation()
+
+/**
+ * Sugar syntax for getting a string value
+ */
 infix fun Context.stringValue(id: Int) = getString(id)
