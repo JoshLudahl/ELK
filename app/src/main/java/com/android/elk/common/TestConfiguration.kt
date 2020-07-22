bd4216b9dbb65af631d8ae0339a5aac8fc22a9e2
Josh Ludahl <josh.ludahl@gmail.com>
2020-07-22 10:43:28 -0700

    configurations

--- app/src/main/java/com/android/elk/common/TestConfiguration.kt
+++ app/src/main/java/com/android/elk/common/TestConfiguration.kt
@@ -3,27 +3,51 @@ package com.android.elk.common
 import android.app.Instrumentation
 import android.content.Context
 import android.content.res.AssetManager
+import android.content.res.Configuration
+import android.content.res.Resources
 import androidx.test.platform.app.InstrumentationRegistry
+import java.util.*
 
 /**
- * Application context.
+ *  Application context
  */
 val targetContext: Context get() = InstrumentationRegistry.getInstrumentation().targetContext
 
 /**
- * Instrumentation context
+ *  Instrumentation context
  */
 val testContext: Context get() = InstrumentationRegistry.getInstrumentation().context
 
 /**
- * AssetManager for androidTest environment
+ *  AssetManager for androidTest environment
  */
 val testAssets: AssetManager = testContext.assets
 
 /**
- * Test runner instrumentation
+ *  Test runner instrumentation
  */
 val instrumentation: Instrumentation get() = InstrumentationRegistry.getInstrumentation()
+
+/**
+ *  Returns
+ */
+fun Context.getConfigurationResources(locale: Locale): Resources {
+    val configuration = Configuration(resources.configuration)
+    configuration.setLocale(locale)
+    return createConfigurationContext(configuration).resources
+}
+
+infix fun Resources.nameOf(viewId: Int): String = getResourceName(viewId)
+
+/**
+ * Gets a application configuration with forced [Locale.GERMAN]
+ */
+fun Context.getGermanConfiguration() = getConfigurationResources(Locale.GERMAN)
+
+/**
+ * Gets a application configuration with forced [Locale.ENGLISH]
+ */
+fun Context.getEnglishConfiguration() = getConfigurationResources(Locale.ENGLISH)
 
 /**
  * Sugar syntax for getting a string value
