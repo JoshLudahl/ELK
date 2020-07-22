204badeededdb72d09944d24d3c51de5886aa360
Josh Ludahl <josh.ludahl@gmail.com>
2020-07-22 15:15:03 -0700

    adding generics

--- app/src/main/java/com/android/elk/common/RobotExample.kt
+++ app/src/main/java/com/android/elk/common/RobotExample.kt
@@ -5,27 +5,54 @@ package com.android.elk.common
  * Lambda for driving navigation. Example: navigate { toLoadboardTab() } will navigate to the
  * LoadBoard tab.
  */
-//fun navigate(robot: NavigationRobot.() -> Unit) = NavigationRobot().apply { robot() }
+class MainView  {
 
-//class NavigationRobot {
-//
-//    fun toDashboardTab() = click on tab(HOME)
-//
-//    fun toLoadBoardTab() = click on tab(LOADBOARD)
-//
-//    fun toWorklistTab() = click on tab(WORKLIST)
-//
-//    fun toMapTab() = click on tab(MAP)
-//
-//    fun toAccountTab() = click on tab(ACCOUNT)
-//}
-//
-//fun selectNavigationTab(tab: Tabs): Matcher<View> {
-//    return when (tab) {
-//        HOME -> withId(R.id.nav_home)
-//        LOADBOARD -> withId(R.id.nav_loadboard)
-//        WORKLIST -> withId(R.id.nav_worklist)
-//        MAP -> withId(R.id.nav_map)
-//        ACCOUNT -> withId(R.id.nav_account)
-//    }
-//}
+    infix fun thenVerify(robot: MainViewResult.() -> Unit): MainViewResult {
+        return MainViewResult().apply { robot() }
+    }
+
+    fun doThisFirst() {
+
+    }
+
+    infix fun goat(init: MainView.() -> Unit) = robotics(init)
+
+}
+
+
+fun verifyNavigation(robot: MainViewResult.() -> Unit) =
+
+    MainViewResult().apply { robot() }
+
+class MainViewResult : Base() {
+
+    fun doSomething() {
+
+    }
+
+    infix fun thenVerify(view: MainView.()-> Unit) = robot(MainView(), view)
+
+}
+class Head(): Base() {
+
+    fun head(init: Head.() -> Unit) = robot(Head(), init)
+
+    fun goat(init: MainView.() -> Unit) = robotics(init)
+}
+
+fun goat(init: MainView.() -> Unit) = robotics(init)
+
+open class Base {
+    fun <T> robot(tag: T, init: T.() -> Unit): T {
+        tag.init()
+        return tag
+    }
+
+    fun <T> robotics(init: T.() -> Unit): T {
+        return init as T
+    }
+}
+
+fun <T> robotics(init: T.() -> Unit): T {
+    return init as T
+}
