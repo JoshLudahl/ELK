4a5a6a7a4513025ed93d1f492e93a21b2231481e
Josh Ludahl <josh.ludahl@gmail.com>
2020-12-18 12:07:08 -0800

    Updated Drawables

--- app/src/main/java/com/android/elk/espresso/CustomDrawableMatchers.kt
+++ app/src/main/java/com/android/elk/espresso/CustomDrawableMatchers.kt
@@ -1,9 +1,18 @@
 package com.android.elk.espresso
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.toColor
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
 
@@ -23,4 +32,30 @@ fun withDrawable(@DrawableRes id: Int) = object : TypeSafeMatcher<View>() {
 
         return view is ImageView && view.drawable.toBitmap().sameAs(expectedBitmap)
     }
+}
+
+fun withDrawable(
+    @DrawableRes id: Int,
+    @ColorRes tint: Int? = null,
+) = object : TypeSafeMatcher<View>() {
+    override fun describeTo(description: Description) {
+        description.appendText("ImageView with drawable same as drawable with id $id")
+        tint?.let { description.appendText(", tint color id: $tint") }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun matchesSafely(view: View): Boolean {
+        val context = view.context
+        val expectedBitmap = context.getDrawable(id)?.tinted(tint)?.toBitmap()
+
+        return view is ImageView && view.drawable.toBitmap().sameAs(expectedBitmap)
+    }
+
+    private fun Drawable.tinted(@ColorInt tintColor: Int? = null) =
+        apply {
+            setTintList(tintColor?.toColorStateList())
+        }
+
+    private fun Int.toColorStateList() = ColorStateList.valueOf(this)
 }
